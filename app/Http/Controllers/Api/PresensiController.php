6dc51b0b61f09fb8ca56d6bd82a19cae4cfd977e
ALFIAN AJI WAHYUDI <alfianajiwahyudi@gmail.com>
2024-12-04 22:43:41 +0700

    update

--- app/Http/Controllers/Api/PresensiController.php
+++ app/Http/Controllers/Api/PresensiController.php
@@ -63,10 +63,10 @@ class PresensiController extends Controller
         $status_scan    = $data['status_scan'];
         $scan           = $data['scan'];
 
-        DB::table('testpresensi')->insert([
-            'desc' => $status_scan
-        ]);
-        die;
+        // DB::table('testpresensi')->insert([
+        //     'desc' => $status_scan
+        // ]);
+        // die;
         // $kode_jam_kerja = $request->kode_jam_kerja;
         $karyawan = DB::table('karyawan')->where('pin', $pin)->first();
         if ($karyawan == null) {
