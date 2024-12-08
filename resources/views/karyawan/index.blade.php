f3e92650c23fbd7d071e43fd99db1d92e4c0e12c
ALFIAN AJI WAHYUDI <alfianajiwahyudi@gmail.com>
2024-12-08 21:58:28 +0700

    update

--- resources/views/karyawan/index.blade.php
+++ resources/views/karyawan/index.blade.php
@@ -116,8 +116,8 @@
                                         <tr>
                                             <th>No</th>
                                             <th>NIK</th>
-                                            <th>Pin</th>
                                             <th>Nama</th>
+                                            <th>Tanggal Lahir</th>
                                             <th>Jabatan</th>
                                             <th>No. HP</th>
                                             <th>Foto</th>
@@ -163,8 +163,8 @@
                                         <tr>
                                             <td>{{ $loop->iteration + $karyawan->firstItem() - 1 }}</td>
                                             <td>{{ $d->nik }}</td>
-                                            <td>{{ $d->pin }}</td>
                                             <td>{{ $d->nama_lengkap }}</td>
+                                            <td>{{ $d->tgl_lahir }}</td>
                                             <td>{{ $d->jabatan }}</td>
                                             <td>{{ $d->no_hp }}</td>
                                             <td>
@@ -402,17 +402,18 @@
                         <div class="col-12">
                             <div class="input-icon mb-3">
                                 <span class="input-icon-addon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-lock-pin">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-calendar">
                                         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                        <path d="M12.5 21h-5.5a2 2 0 0 1 -2 -2v-6a2 2 0 0 1 2 -2h10c.24 0 .47 .042 .683 .12" />
-                                        <path d="M11 16a1 1 0 1 0 2 0a1 1 0 0 0 -2 0" />
-                                        <path d="M8 11v-4a4 4 0 1 1 8 0v4" />
-                                        <path d="M21.121 20.121a3 3 0 1 0 -4.242 0c.418 .419 1.125 1.045 2.121 1.879c1.051 -.89 1.759 -1.516 2.121 -1.879z" />
-                                        <path d="M19 18v.01" />
+                                        <path d="M4 7a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12z" />
+                                        <path d="M16 3v4" />
+                                        <path d="M8 3v4" />
+                                        <path d="M4 11h16" />
+                                        <path d="M11 15h1" />
+                                        <path d="M12 15v3" />
                                     </svg>
                                 </span>
-                                <input type="text" id="pin" value="" class="form-control"
-                                    name="pin" placeholder="pin">
+                                <input type="date" id="tgl_lahir" value="" class="form-control"
+                                    name="tgl_lahir" placeholder="Tanggal Lahir">
                             </div>
                         </div>
                     </div>
